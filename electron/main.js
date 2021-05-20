2c29182ef876eaa7dfc77700211fad851463399a
秦燕 <2640855495@qq.com>
2021-05-20 20:26:28 +0800

    add basic code

---
+++ electron/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow, Tray } = require('electron');
+const path = require('path');
+const server_ = require("../server/server");
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+
+function createWindow () {
+    let dictServer = new server_.dictServer();
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+    win.setTitle("Ducky Dictionary");
+    
+    // win.setMenu(null);
+    win.loadURL("http://127.0.0.1:14514/");
+};
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        };
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    };
+});
