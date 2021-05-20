be4f189ebac67e772bceb7bf1dd9da9512fa8d32
秦燕 <2640855495@qq.com>
2021-05-20 22:17:36 +0800

    添加了个能看的界面。单词页是从以前的项目中复制的，用了less来给css选择器批量加上content

--- electron/main.js
+++ electron/main.js
@@ -14,7 +14,7 @@ function createWindow () {
     });
     win.setTitle("Ducky Dictionary");
     
-    // win.setMenu(null);
+    win.setMenu(null);
     win.loadURL("http://127.0.0.1:14514/");
 };
 
