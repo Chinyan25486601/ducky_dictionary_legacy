18bfe883e685f49add8191f321fbe80f953a18f9
秦燕 <2640855495@qq.com>
2021-06-04 22:58:53 +0800

    基本确定了词库json的最终格式并制作了一个最小化单行中考词库和一个示例用的只有10个单词的格式化过的json

--- dict_source/zk_word_screen.py
+++ dict_source/zk_word_screen.py
@@ -3,11 +3,11 @@ import json
 import re
 
 dest = []
+i=0
 
 with open("./ecdict/ecdict.csv", encoding="utf-8") as csvfile:
     r = csv.reader(csvfile)
     r.__next__()
-    i=1
     for w in r:
         # TEST ----------------
         # if i>10:
@@ -16,15 +16,28 @@ with open("./ecdict/ecdict.csv", encoding="utf-8") as csvfile:
         if not "zk" in w[7]:
             continue
         else:
-            w_ = list(w).copy()
+            w_ = list(w)
             w_[2] = w_[2].replace("\\r\\n","\n")
             w_[2] = w_[2].replace("\\n","\n")
             w_[3] = w_[3].replace("\\r\\n","\n")
             w_[3] = w_[3].replace("\\n","\n")
             if w_[11]=='""':
                 w_[11]=""
-            print(w_[2])
+            def parse_definition(rawstr):
+                destination=list()
+                rawstr_lines=rawstr.split("\n")
+                type_rule = re.compile(r"^([a-zA-Z]+\. )")
+                for line in rawstr_lines:
+                    split_result = [x.strip() for x in type_rule.split(line)]
+                    if len(split_result)==1:
+                        split_result.insert(0,"")
+                        split_result.insert(0,"")
+                    destination.append(split_result[1:3])
+                return destination
+            w_[2]=parse_definition(w_[2])
+            w_[3]=parse_definition(w_[3])
             dest.append({
+                "id": i,
                 "word": w_[0],
                 "phonetic": w_[1],
                 "definition": w_[2],
@@ -32,16 +45,19 @@ with open("./ecdict/ecdict.csv", encoding="utf-8") as csvfile:
                 "pos": w_[4],
                 "collins": w_[5],
                 "oxford": w_[6],
-                "tag": w_[7],
+                "tag": w_[7].split(" "),
                 "bnc": w_[8],
                 "frq": w_[9],
-                "exchange": w_[10],
+                "exchange": [x.split(":") for x in w_[10].split("/")],
                 "detail": w_[11]
             })
             # break
             i+=1
 
-print(i)
+print(i+1)
 print(dest[0]["definition"])
 with open("zk_words.json", "w",encoding="utf-8") as zkfile:
-    zkfile.write(json.dumps(dest, indent=4,ensure_ascii=False))
+    # dev
+    # zkfile.write(json.dumps(dest, indent=4,ensure_ascii=False))
+    
+    zkfile.write(json.dumps(dest, ensure_ascii=False))
