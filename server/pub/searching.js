b79d2a2d323248b2cab9f4f4ae212e907b198d05
秦燕 <2640855495@qq.com>
2021-06-05 18:51:26 +0800

    添加了冠词的标签支持

--- server/pub/searching.js
+++ server/pub/searching.js
@@ -4,7 +4,6 @@ let search_icon = document.getElementById("search_icon");
 let search_big = document.getElementById("search_big");
 let search_icon_big = document.getElementById("search_icon_big");
 let search_content_temp = "";
-let word_data = {};
 let exchange_table = [
     ["p","Past Tense"],
     ["d","Past Participle"],
@@ -38,10 +37,90 @@ let meaning_type_class_table = [
     ["conj","conjunction"],
     ["r","adverb"],
     ["s","adjective"],
+    ["art.","article"],
+    ["art","article"]
 ]
+let nowWordId = 0;
 
-
-var findWord = function (query="-1") {
+let installWordData = function(word_data){
+    document.querySelector(".word_id").innerText=word_data.id.toString();
+    document.querySelector(".word_title").innerText=word_data.word;
+    document.querySelector(".IPA").innerText=word_data.phonetic;
+    let derivatives = document.querySelector(".derivatives");
+    derivatives.innerHTML="";
+    word_data.exchange.forEach(e=>{
+        if((e[0]!="1") && (e[0]!="") && (e[1]!=undefined) && (e[0]!="f")) {
+            try{exchange_table.forEach(t=>{
+                let e0 = e[0];
+                e[0]=e[0].replace(t[0],t[1]);
+                if(e0!=e[0]){
+                    throw new Error("break");
+                }
+            });}catch(e){
+                if (e.message !== "break") throw e;
+            }
+            let derivative_type = document.createElement("span");
+            derivative_type.classList.add("derivative_type");
+            derivative_type.innerText=e[0];
+            let derivative_word = document.createElement("a");
+            derivative_word.classList.add("derivative_word");
+            derivative_word.innerText=e[1];
+            let derivative = document.createElement("div");
+            derivative.classList.add("derivative");
+            derivative.appendChild(derivative_type);
+            derivative.appendChild(derivative_word);
+            derivatives.appendChild(derivative);
+        };
+    });
+    let lastType = ""
+    let meanings = document.querySelector(".meanings");
+    meanings.innerHTML="";
+    let meaning_id = 1;
+    word_data.translation.forEach(t=>{
+        if(true){
+            let meaning = document.createElement("div");
+            let meaningType = document.createElement("span");
+            let meaning_description = document.createElement("span");
+            let meaning_t = "";
+            try{meaning_type_class_table.forEach(m=>{
+                let t0 = t[0];
+                meaning_t=t[0].replace(m[0],m[1]);
+                if(t0!=meaning_t){
+                    throw new Error("break");
+                }
+            });}catch(e){
+                if (e.message !== "break") throw e;
+            };
+            
+            if(meaning_t!="") {
+                meaningType.classList.add(meaning_t);
+                meaningType.classList.add("meaning_type");
+            }else{
+                meaningType.classList.add("hidden");
+            }
+            meaningType.innerText = t[0];
+            if(t[0]==lastType) {
+                meaningType.classList.add("hidden")
+            } else {
+                meaning.style.marginTop="5px";
+                lastType=t[0];
+                meaning_description.classList.add("strong");
+            };
+            
+            meaning_description.classList.add("meaning_description");
+            meaning_description.innerText = meaning_id.toString()+". "+t[1]
+            meaning.appendChild(meaningType);
+            meaning.appendChild(meaning_description);
+            meanings.appendChild(meaning);
+            meaning_id+=1;
+        }
+    })
+    switchContent(contentStatus.word_page);
+}
+let getWordDataById = function(id){
+    
+}
+let findWord = function (query="-1") {
     // switchContent(contentStatus.search_page);
     search_big.innerText = search_content_temp;
     //TODO:添加搜索处理机制
@@ -54,81 +133,7 @@ var findWord = function (query="-1") {
             return response.json()
         })
         .then(myJson=>{
-            word_data=myJson;
-            document.querySelector(".word_id").innerText=word_data.id.toString();
-            document.querySelector(".word_title").innerText=word_data.word;
-            document.querySelector(".IPA").innerText=word_data.phonetic;
-            let derivatives = document.querySelector(".derivatives");
-            derivatives.innerHTML="";
-            word_data.exchange.forEach(e=>{
-                if((e[0]!="1") && (e[0]!="") && (e[1]!=undefined) && (e[0]!="f")) {
-                    try{exchange_table.forEach(t=>{
-                        let e0 = e[0];
-                        e[0]=e[0].replace(t[0],t[1]);
-                        if(e0!=e[0]){
-                            throw new Error("break");
-                        }
-                    });}catch(e){
-                        if (e.message !== "break") throw e;
-                    }
-                    let derivative_type = document.createElement("span");
-                    derivative_type.classList.add("derivative_type");
-                    derivative_type.innerText=e[0];
-                    let derivative_word = document.createElement("a");
-                    derivative_word.classList.add("derivative_word");
-                    derivative_word.innerText=e[1];
-                    let derivative = document.createElement("div");
-                    derivative.classList.add("derivative");
-                    derivative.appendChild(derivative_type);
-                    derivative.appendChild(derivative_word);
-                    derivatives.appendChild(derivative);
-                };
-            });
-            let lastType = ""
-            let meanings = document.querySelector(".meanings");
-            meanings.innerHTML="";
-            let meaning_id = 1;
-            word_data.translation.forEach(t=>{
-                if(true){
-                    let meaning = document.createElement("div");
-                    let meaningType = document.createElement("span");
-                    let meaning_description = document.createElement("span");
-                    let meaning_t = "";
-                    try{meaning_type_class_table.forEach(m=>{
-                        let t0 = t[0];
-                        meaning_t=t[0].replace(m[0],m[1]);
-                        if(t0!=meaning_t){
-                            throw new Error("break");
-                        }
-                    });}catch(e){
-                        if (e.message !== "break") throw e;
-                    };
-                    
-                    if(meaning_t!="") {
-                        meaningType.classList.add(meaning_t);
-                        meaningType.classList.add("meaning_type");
-                    }else{
-                        meaningType.classList.add("hidden");
-                    }
-                    meaningType.innerText = t[0];
-                    if(t[0]==lastType) {
-                        meaningType.classList.add("hidden")
-                    } else {
-                        meaning.style.marginTop="5px";
-                        lastType=t[0];
-                        meaning_description.classList.add("strong");
-                    };
-                    
-                    meaning_description.classList.add("meaning_description");
-                    meaning_description.innerText = meaning_id.toString()+". "+t[1];
-
-                    meaning.appendChild(meaningType);
-                    meaning.appendChild(meaning_description);
-                    meanings.appendChild(meaning);
-                    meaning_id+=1;
-                }
-            })
-            switchContent(contentStatus.word_page);
+            installWordData(myJson);
         });
 };
 
